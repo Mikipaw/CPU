0267793af25c5eb7b367de9703a5f2447a245d1b
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-13 15:22:52 +0300

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -5,6 +5,9 @@
 #ifndef PROC_CPU_H
 #define PROC_CPU_H
 
+#ifndef PROC_CPU_H
+#define PROC_CPU_H
+
 #include "Stack.h"
 #include "myStr.h"
 
@@ -14,6 +17,7 @@ enum List_of_mistakes{
     ERROR_NULL_PTR,         //2
     ERROR_EMPTY_FILE,       //3
     ERROR_UNKNOWN_COMMAND,  //4
+    EXIT_CODE,              //99
 };
 
 /*!
@@ -34,27 +38,85 @@ bool Is_push(const char* str);
 
 int Do_command(Stack<double>* stk, simple_string command){
     int command_hash = charsum(command.string);
-    if (!Is_push(command.string)){
+    double first  = 0;
+    double second = 0;
+
+    if (command_hash != 320){
         switch (command_hash) {
-            case 201:
+            case 201:                               //ADD
+                stk->pop    (&first);
+                stk->pop    (&second);
+                stk->push   (first+second);
+                break;
 
-            case 234:
+            case 234:                               //SUB
+                stk->pop    (&first);
+                stk->pop    (&second);
+                stk->push   (second - first);
+                break;
 
-            case 238:
+            case 238:                               //MUL
+                stk->pop    (&first);
+                stk->pop    (&second);
+                stk->push   (first * second);
+                break;
+
             case 227:
+                stk->pop    (&first);
+                stk->pop    (&second);
+                stk->push   (second / first);
+
             case 218:
+                stk->pop(&first);
+                stk->push(-first);
+                break;
+
             case 402:
+                stk->pop(&first);
+                stk->push(sin(first));
+                break;
+
             case 229:
+                stk->pop(&first);
+                stk->push(cos(first));
+                break;
+
             case 330:
+                stk->pop(&first);
+                stk->push(sqrt(first));
+                break;
+
             case 246:
-            case 320:
+                stk->pop(&first);
+                stk->pop(&second);
+                stk->push(pow(second, first));
+                break;
+
             case 239:
+                stk->pop();
+                break;
+
             case 151:
+                printf("Please, enter your number:\n");
+                scanf("%lf", first);
+                stk->push(first);
+                break;
+
             case 248:
+                stk->pop(&first);
+                printf("Result = %lf\n", first);
+                break;
+
             case 310:
+                Stack_info(*stk);
+                break;
+
             case 232:
+                printf("CPU is shutting down...\n");
+                return EXIT_CODE;
 
             default:
+                printf("Unknown command!\n");
                 return ERROR_UNKNOWN_COMMAND;
         }
     }
@@ -67,6 +129,7 @@ int Do_command(Stack<double>* stk, simple_string command){
         double dnum = strtod(num, &end);
         stk->push(dnum);
     }
+
     return ALL_OK;
 }
 
@@ -78,6 +141,8 @@ bool Is_push(const char* str){
     return str[1] == 'U' && str[2] == 'S' && str[3] == 'H' && str[4] == ' ';
 
 }
+
+
 
 
 #endif //PROC_CPU_H
