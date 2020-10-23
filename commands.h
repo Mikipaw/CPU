ab2b9d12894e0ff5e94d127189ead7db8bc3705e
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-10-23 14:18:09 +0300

    Update commands.h

--- commands.h
+++ commands.h
@@ -2,84 +2,91 @@
 // Created by mikipaw on 16.10.2020.
 //
 
-
-
-#define DO_PUSH{                    \
-fscanf(input, "%lf", &first);       \
-cpu_stack.push(first);              \
-                                    \
+#define DO_PUSH {                           \
+fscanf(input, "%d", &integ);                \
+if(integ == 0){                             \
+    fscanf(input, "%lf", &second);          \
+    cpu_stack.push(second);                 \
+    }                                       \
+    else {                                  \
+        cpu_stack.push(reg[integ]);         \
+        reg[integ] = 0;                     \
+    }                                       \
 };
 
-#define DO_POP{                     \
-cpu_stack.pop(&first);              \
+#define DO_POP {                        \
+fscanf(input, "%d", &integ);            \
+if(integ == 0) cpu_stack.pop();         \
+else cpu_stack.pop(&reg[integ]);        \
 };
 
-#define DO_ADD{                     \
+#define DO_ADD {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.pop(&second);             \
 cpu_stack.push(first + second);     \
 };
 
-#define DO_SUB{                     \
+#define DO_SUB {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.pop(&second);             \
 cpu_stack.push(first - second);     \
 };
 
-#define DO_MUL{                     \
+#define DO_MUL {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.pop(&second);             \
 cpu_stack.push(first * second);     \
 };
 
-#define DO_DIV{                     \
+#define DO_DIV {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.pop(&second);             \
 cpu_stack.push(first / second);     \
 };
 
-#define DO_NEG{                     \
+#define DO_NEG {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.push(-first);             \
 };
 
-#define DO_SINUS{                   \
+#define DO_SINUS {                  \
 cpu_stack.pop(&first);              \
 cpu_stack.push(sin(first));         \
 };
 
-#define DO_COS{                     \
+#define DO_COS {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.push(cos(first));         \
 };
 
-#define DO_SQRT{                    \
+#define DO_SQRT {                   \
 cpu_stack.pop(&first);              \
 cpu_stack.push(sqrt(first));        \
 };
 
-#define DO_POW{                     \
+#define DO_POW {                    \
 cpu_stack.pop(&first);              \
 cpu_stack.pop(&second);             \
 cpu_stack.push(pow(first, second)); \
 };
 
-#define DO_IN{                      \
+#define DO_IN {                     \
 printf("\nInput: \n");              \
 scanf("%lf", first);                \
 cpu_stack.push(first);              \
 };
 
-#define DO_OUT{                     \
+#define DO_OUT {                    \
 cpu_stack.pop(&first);              \
 printf("\nOutput: %lf\n", first);   \
 };
 
-#define DO_DUMP{                    \
+#define DO_DUMP {                   \
+CPU_info(*this);                    \
 Stack_info(cpu_stack);              \
 };
 
-#define DO_HLT{                     \
+#define DO_HLT {                    \
 printf("\nProcess finishing...\n"); \
 return;                             \
 };
@@ -99,21 +106,3 @@ DEF_CMD( IN,    11,   {DO_IN})
 DEF_CMD( OUT,   12,   {DO_OUT})
 DEF_CMD( DUMP,  13,   {DO_DUMP})
 DEF_CMD( HLT,   14,   {DO_HLT})
-
-/*
-DEF_CMD( PUSH,  9)
-DEF_CMD( POP,   10)
-DEF_CMD( ADD,   0)
-DEF_CMD( SUB,   1)
-DEF_CMD( MUL,   2)
-DEF_CMD( DIV,   3)
-DEF_CMD( NEG,   4)
-DEF_CMD( SINUS, 5)
-DEF_CMD( COS,   6)
-DEF_CMD( SQRT,  7)
-DEF_CMD( POW,   8)
-DEF_CMD( IN,    11)
-DEF_CMD( OUT,   12)
-DEF_CMD( DUMP,  13)
-DEF_CMD( HLT,   14)
-*/
