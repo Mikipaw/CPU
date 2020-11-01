e1ba5d13c031139b56fbf77e9563d7c8d81a2df4
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-11-02 00:35:44 +0300

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -7,9 +7,16 @@
 
 #include "Stack.h"
 #include "enum.h"
+#include "cstring"
+
+#include "myStr.h"
 
 class CPU;
 
+/*!
+ * @enum Registers
+ * @brief Registers in CPU.
+ */
 enum Registers{
     RAX,        //0
     RBX,        //1
@@ -19,16 +26,26 @@ enum Registers{
 
 #define CREATE_CPU(name, number_of_commands, id) CPU name(number_of_commands, id, #name);
 
+
+/*!
+ * @function void CPU_info(const CPU& cpu);
+ * @param cpu - const CPU&
+ * @brief Function prints some information in CPU.
+ * @return nothing.
+ */
 void CPU_info(const CPU& cpu);
 
-/*! \class CPU
- *  \brief This CPU executes commands from file with codes of coammnds.
+//TODO: copy constructor
+//TODO: delete operator
+/*! @class CPU
+ *  @brief This CPU executes commands from file with codes of coammnds.
  *
- *  \param cpu_stack - stack with double numbers.
- *  \param number_of_commands - number of commands in input file with commands.
- *  \param id - id.
- *  \param version - version.
- *  \param name - name of the CPU.
+ *  @param cpu_stack - stack with double numbers.
+ *  @param number_of_commands - number of commands in input file with commands.
+ *  @param id - id.
+ *  @param version - version.
+ *  @param name - name of the CPU.
+ *  @param ret - pointer to return back from function.
  *
  *  Some details about the CPU class.
  */
@@ -37,26 +54,31 @@ private:
     Stack<double> cpu_stack = Stack<double>(1);
     size_t number_of_commands = 0;
     const char* name = "Unnamed CPU";
+
+
     double* reg     = nullptr;
     double* array   = nullptr;
     int* mark       = nullptr;
 
+    int ret = 0;
     int id = 0;
     const int version = 2;
 
 public:
     explicit
     CPU(size_t num_of_coms, int my_id, const char* new_name):
-    number_of_commands      (num_of_coms),
-    id                      (my_id),
-    reg                     (new double[4]),
-    cpu_stack               (1, "CPU Stack"),
-    mark                    (new int[4]),
-    name                    (new_name){
-        for(int i = 0; i < 4; ++i) {
-            reg[i] = 0;
-            mark[i] = 0;
-        }
+        number_of_commands      (num_of_coms),
+        id                      (my_id),
+        reg                     (new double[5]),
+        cpu_stack               (1, "CPU Stack"),
+        mark                    (new int[5]),
+        name                    (new_name)
+        {
+            for(int i = 0; i < 5; ++i) {
+                reg[i] = 0;
+                mark[i] = 0;
+            }
+
         FILE* input = fopen("Output_file.txt", "rb");
         assert(input != nullptr);
 
@@ -72,11 +94,14 @@ public:
         int i = 0;
         int j = 0;
         size_t lenstr = strlen(text);
+
         while(i < lenstr) {
             sscanf(text + i, "%lf", &doub);
             array[j++] = doub;
-            i+=(2 + ((int) doub)/10);
+            i+=2;
+            if(doub >= 10) i++;
         }
+
         number_of_commands = non;
     };
 
@@ -90,45 +115,25 @@ public:
     int
     Work();
 
+    void Change_ret(int pointer){ ret = pointer; }
+
     [[nodiscard]] const char* Get_name()    const { return name; };
     [[nodiscard]] int Get_version()         const { return version; };
     [[nodiscard]] int Get_id()              const { return id; };
+    [[nodiscard]] int Get_ret()             const { return ret; };
 
     [[nodiscard]] double Get_reg (int number) const { return reg[number]; };
 
     ~CPU(){
         free(reg);
+        //nullptr
         free(array);
         free(mark);
     }
 
 };
 
-
-int CPU::Work(){
-    double first    = 0;
-    double second   = 0;
-    int    integ    = 0;
-    for(int i = 0; i <= number_of_commands; ++i) {
-        integ = (int) array[i];
-        switch (integ) {
-        #define DEF_CMD(name, num, code){                       \
-                    case num:                                   \
-                    code;                                       \
-                    break;                                      \
-                }
-        #include "commands.h"
-
-        #undef DEF_CMD
-            default:
-                return ERROR_UNKNOWN_COMMAND;
-        }
-    }
-
-    return ALL_OK;
-}
-
-
+//TODO: байты
 void CPU_info(const CPU& cpu){
     printf("CPU = [%p], \"%s\"\n", &cpu, cpu.Get_name());
     printf("{\n");
@@ -137,13 +142,38 @@ void CPU_info(const CPU& cpu){
     printf("\tRegisters:\n");
     printf("\t{\n");
 
-        printf("\t\tRAX = %lf\n", cpu.Get_reg(RAX));
-        printf("\t\tRBX = %lf\n", cpu.Get_reg(RBX));
-        printf("\t\tRCX = %lf\n", cpu.Get_reg(RCX));
-        printf("\t\tRDX = %lf\n", cpu.Get_reg(RDX));
+    printf("\t\tRAX = %lf\n", cpu.Get_reg(RAX));
+    printf("\t\tRBX = %lf\n", cpu.Get_reg(RBX));
+    printf("\t\tRCX = %lf\n", cpu.Get_reg(RCX));
+    printf("\t\tRDX = %lf\n", cpu.Get_reg(RDX));
 
     printf("\t}\n");
 }
+
+
+int CPU::Work(){
+    double first    = 0;
+    double second   = 0;
+    int    integ    = 0;
+    for(int i = 0; i < number_of_commands; ++i) {
+        integ = (int) array[i];
+        switch (integ) {
+        #define DEF_CMD(name, num, code, hash){                         \
+                    case num:                                           \
+                    code;                                               \
+                    break;                                              \
+                }
+    #include "commands.h"
+
+    #undef DEF_CMD
+            default:
+                return ERROR_UNKNOWN_COMMAND;
+        }
+    }
+
+    return ALL_OK;
+}
+
 
 
 #endif //PROC_CPU_H
