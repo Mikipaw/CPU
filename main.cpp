d7ecccd13634f79085b9b6c4be986e3dcc3c1236
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-11-05 21:09:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,51 +11,36 @@
  *  \copyright GNU Public License.
  */
 
+#include "Assembler.h"
 #include "CPU.h"
+#include "Disassembler.h"
 
-
+#define asserted || printf("Assertion failed on line %d\n", __LINE__)
 
 int Interface();
 
 int main() {
-    int code = 0;//Interface();
-    int s = charsum("HLT");
-    printf("%d\n", s);
-    return code;
+    int code = Interface();
+    //PrintTextFile();
+    return 0;
 }
 
 
 int Interface(){
+    int number_of_cmds = 6;
+    Assembler(&number_of_cmds);
+    ASM_Listing();
 
-    FILE* command_file = fopen("command_file.txt", "rb");
-    if (command_file == nullptr) {
-        fprintf(stderr, "Error opening file!\n");
-        return ERROR_OPENING_FILE;
+
+    CREATE_CPU(proc, number_of_cmds, 1);
+    CPU_info(proc);
+    int check = proc.Work();
+    if(check != 0) {
+        CPU_info(proc);
+        return check;
     }
+    Disassembler();
 
-    size_t size_of_file         = Size_of_file(command_file);
-    size_t number_of_commands   = 0;
-
-    char* command_text = text_from_file(command_file, size_of_file, &number_of_commands);
-    if (command_text == nullptr) return ERROR_NULL_PTR;
-    if (number_of_commands == 0) return ERROR_EMPTY_FILE;
-
-    simple_string* commands = (simple_string*) calloc(number_of_commands + 1, sizeof(simple_string));
-    commands[0].string = command_text;
-
-    Arrange_str_ptrs(commands, number_of_commands, command_text);
-    fclose (command_file);
-
-    Stack<double> stak(1);
-
-    int com_code = 0;
-    for (int i = 0; i <= number_of_commands; ++i){
-        com_code = Do_command(&stak, commands[i]);
-        if (com_code != 0) return com_code;
-    }
-
-
-
-
+    size_t t = 0;
     return ALL_OK;
 }
